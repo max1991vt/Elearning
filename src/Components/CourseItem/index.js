42f17a8b0f01999558cc31e3a1e74857b35f7b75
huy hoang <hoangtronghuy91@gmail.com>
2020-10-10 21:19:25 +0700

    'loadcourseList'

--- src/Components/CourseItem/index.js
+++ src/Components/CourseItem/index.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react'
 export default class CourseItem extends Component {
     render() {
         return (
-            <div classname="card p-2">
-                <img></img>
-                <p className="lead font-weight-bold">ReactJS BootCamp</p>
-                <p classname="lead">Instructor:</p>
-                <p classname="lead">Rating: 5.0</p>
-                <button className="btn btn-success">Go to detail</button>
+            <div classname="card p-2 mb-4">
+                <img src={this.props.item.hinhAnh} style={{width:"100%",height:280}}></img>
+        <p className="lead font-weight-bold ">{this.props.item.tenKhoaHoc}</p>
+                <p classname="lead ">Instructor: {this.props.item.nguoiTao.hoTen}</p>
+                <p classname="lead ">Rating: 5.0</p>
+                <button className="btn btn-success ">Go to detail</button>
                 
             </div>
 
--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -14,7 +14,7 @@ class HomeScreen extends Component {
                     <div classname="row">
                         {this.props.courseList.map((item, index) => (
                                 <div className="col-3">
-                                    <CourseItem />
+                                    <CourseItem item={item}/>
                                 </div>
                             ))}
 
@@ -28,8 +28,9 @@ class HomeScreen extends Component {
     componentDidMount() {
         Axios({
             method: "GET",
-            url: 'https://elearning0706.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayDanhSachKhoaHoc?MaNhom=GP04'
+            url: 'http://elearning0706.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayDanhSachKhoaHoc?MaNhom=GP04'
         }).then((res) => {
+            
             this.props.dispatch({
                 type: 'FETCH_COURSES',
                 payload: res.data
